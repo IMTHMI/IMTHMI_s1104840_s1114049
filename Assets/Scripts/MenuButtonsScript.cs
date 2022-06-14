819da9c4b566ea499f4a658092b9b7db2b0035f6
gijsbuis <gijs.buis@me.com>
2022-06-14 14:48:45 +0200

    Added text for showing sizes of deskmat

--- Assets/Scripts/MenuButtonsScript.cs
+++ Assets/Scripts/MenuButtonsScript.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MenuButtonsScript : MonoBehaviour
 {
     public GameObject objectToSpawn;
+    [SerializeField] TextMeshPro sizesText;
 
     public void Start()
     {
@@ -14,6 +16,7 @@ public class MenuButtonsScript : MonoBehaviour
     public void NieuweMat()
     {
         objectToSpawn.SetActive(true);
+        sizesText.text = "1500mm" + "x" + "1500mm";
         // Instantiate(objectToSpawn, GameObject.Find("Spawner").transform.position, GameObject.Find("Spawner").transform.rotation);
     }
 
